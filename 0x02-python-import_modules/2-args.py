bb6c3d81fe56ce1a38bcb9cc4b6703e2616bb6dd
Daniel Kigen Buturu <dbuturu@gmail.com>
2022-06-02 20:04:02 +0300

    Updated 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,18 @@
 from sys import argv
 
 if __name__ == "__main__":
+    amount = "argument"
+
+    if len(argv) > 1 or len(argv) == 0:
+        amount += "s"
+
+    if len(argv) == 0:
+        amount += "."
+    else:
+        amount += ":"
+
     print(f"{len(argv)} arguments:")
+    
     for i, arg in enumerate(argv):
         if i == 0:
             continue
