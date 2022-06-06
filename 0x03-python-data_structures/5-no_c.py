e0b16da694a65866de7aaa108a81b0ed615ad08e
Daniel Kigen Buturu <dbuturu@gmail.com>
2022-06-06 19:52:33 +0300

    Updated 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    copy = "".join([s for s in my_string if s != 'c' and s != 'C'])
+    copy = ""
+    for s in my_string:
+        if s != 'c' and s != 'C':
+            copy += s
     return copy
