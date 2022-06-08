5aabc3e49ecd9a9a06d52f1d470d3cf52e8ce4fb
Daniel Kigen Buturu <dbuturu@gmail.com>
2022-06-08 21:40:13 +0300

    Added 10-best_score.py

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+        return None
+
+    return sorted(a_dictionary.items(), key=lambda x: x[1], reverse=True)[0][0]
